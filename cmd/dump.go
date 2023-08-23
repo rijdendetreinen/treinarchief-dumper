be63528e6cc2e4f52e53fbecf43b0511f30b5f4b
Geert Wirken <geert@gwirken.nl>
2023-08-23 15:00:58 +0200

    Initial commit

---
+++ cmd/dump.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/rijdendetreinen/treinarchief-dumper/db"
+	"github.com/spf13/cobra"
+)
+
+var dumpCmd = &cobra.Command{
+	Use:   "dump",
+	Short: "Dump",
+}
+
+var dumpDayCmd = &cobra.Command{
+	Use:   "day",
+	Short: "Dump a single day",
+	Run: func(cmd *cobra.Command, args []string) {
+		database := db.CreateDB()
+
+		db.Dump(database, args[0], args[0])
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(dumpCmd)
+	dumpCmd.AddCommand(dumpDayCmd)
+}
